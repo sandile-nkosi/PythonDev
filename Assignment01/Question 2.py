98b8aa94449a7f05b82664e7794de2edb0580020
Sandile <83593288+sandile-nkosi@users.noreply.github.com>
2022-11-23 13:00:51 +0200

    Assignment question 2 completed

---
+++ Assignment01/Question 2.py
@@ -0,0 +1,11 @@
+# Question two
+# Write a Python program that prints the length of a string.
+# For example of expected results;
+# Input Output Explanation
+# "" 0 0 because its empty string
+# “Jambo” 5 5 because it has 5 characters
+# “Power Learn Project (PLP)” ? ?
+
+word = input("This program will print the length of a string. Enter a string: ")
+
+print(len(word))
